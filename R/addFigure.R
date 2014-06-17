db4b9a8432f3d92aae1e28ed1db0e2f66d617054
mikeck <michael.koohafkan@gmail.com>
2014-06-17 16:12:06 -0700

    major version jump to 1.0! This marks the end of major development of figr (I will still fix bugs as they are found). I am switching my efforts to figr2, a streamlined version of figr designed for tight integration with knitr.

--- R/addFigure.R
+++ R/addFigure.R
@@ -8,6 +8,18 @@ figure,
 caption
 ### the figure caption (optional)
 ){
+  if(is.character(figure)){
+    # copy external file to knitr's folder
+    newpath <- paste(opts_chunk$get('fig.path'), basename(figure), sep="")
+    if(file.exists(newpath))
+      stop("knitr figure ", newpath, "Already exists.")
+	# if copy fails, assume it's a URL
+    if(!file.copy(figure, newpath)){
+	  file.create(newpath)
+	  download.file(figure, newpath, quiet=TRUE)
+	}
+    figure <- newpath
+  }
   add(key, figure, caption, type='Figure')
-### Figure is indexed. No return value
+### Figure is registered. No return value
 }
